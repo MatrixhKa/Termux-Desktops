ca3150233e8128c943b09b9fe8def5d69dc5ad9d
MatrixhKa <123204683+MatrixhKa@users.noreply.github.com>
2025-04-12 13:52:25 -0300

    Update startxfce4_chrootArch.sh

--- scripts/chroot/arch/startxfce4_chrootArch.sh
+++ scripts/chroot/arch/startxfce4_chrootArch.sh
@@ -4,7 +4,7 @@ killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-
 
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
 
-sudo busybox mount --bind $PREFIX/tmp /data/local/tmp/chrootarch/tmp
+sudo busybox mount --bind $PREFIX/tmp /data/local/tmp/chrootArch/tmp
 
 XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :0 -ac &
 
